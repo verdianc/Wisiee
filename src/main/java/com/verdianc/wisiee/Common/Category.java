2e8184f0a5053b49632fe1f2d9aeb8ea3049a97c
suahlingo <sgua0720@gmail.com>
2025-08-17 15:56:54 +0900

    🚀 : enum 클래스 추가

---
+++ src/main/java/com/verdianc/wisiee/Common/Category.java
@@ -0,0 +1,10 @@
+package com.verdianc.wisiee.Common;
+
+public enum Category {
+
+  GOODS_EXCHANGE,        // 굿즈 교환
+  GOODS_PROXY_PURCHASE,  // 굿즈 대리 구매
+  GOODS_SALE,            // 굿즈 판매
+  ALBUM_GOODS_GROUP_BUY  // 앨범/굿즈 공동 구매
+
+}
