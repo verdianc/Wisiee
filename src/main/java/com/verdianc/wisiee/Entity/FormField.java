ec309b45af729dc42255b55a32db41eb4c071fda
minjeong204 <anjell1218@gmail.com>
2025-08-17 14:28:39 +0900

    🚀 FormField Entity 추가

--- src/main/java/com/verdianc/wisiee/Entity/FormField.java
+++ src/main/java/com/verdianc/wisiee/Entity/FormField.java
@@ -4,10 +4,24 @@ import java.util.List;
 
 public class FormField {
   // 폼 영역(일부) -> 폼인포에 여러개 들어감
-
+  private String formId;
   //enum, 단일인지, 다중인지
   // dto에서 리스트로 받아서 여러개 넣기
   private String selectOption;
+
+  private List<String> optionInfos;
+
+  private String title;
+
+  private String descipt;
+
+  private Boolean required;
+
+  private int order;
+
+
+
+
 
 
 
