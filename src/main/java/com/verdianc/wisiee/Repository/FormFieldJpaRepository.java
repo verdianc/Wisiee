1155c19a91aacfbefbefd492b97fa121fbf821ca
minjeong204 <anjell1218@gmail.com>
2025-08-20 23:45:24 +0900

    🚧 : form Id Long으로 변경, 복합키 관련 어노테이션 추가 및 변경

--- src/main/java/com/verdianc/wisiee/Repository/FormFieldJpaRepository.java
+++ src/main/java/com/verdianc/wisiee/Repository/FormFieldJpaRepository.java
@@ -4,7 +4,9 @@ import com.verdianc.wisiee.Entity.FormField;
 import com.verdianc.wisiee.Entity.FormFieldId;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FormFieldJpaRepository extends JpaRepository<FormField, FormFieldId> {
     // formId 기준으로 전부 조회 + order 순으로 정렬
     List<FormField> findByFormFieldId_FormId(Long formId);
