b785eca10a91d52b9811af49e74629c3e6eaedb6
minjeong204 <anjell1218@gmail.com>
2025-08-20 19:53:30 +0900

    🚀 : REPOSITORY 함수 추가

--- src/main/java/com/verdianc/wisiee/Repository/FormFieldJpaRepository.java
+++ src/main/java/com/verdianc/wisiee/Repository/FormFieldJpaRepository.java
@@ -2,7 +2,13 @@ package com.verdianc.wisiee.Repository;
 
 import com.verdianc.wisiee.Entity.FormField;
 import com.verdianc.wisiee.Entity.FormFieldId;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FormFieldJpaRepository extends JpaRepository<FormField, FormFieldId> {
+    // formId 기준으로 전부 조회 + order 순으로 정렬
+    List<FormField> findByFormFieldId_FormIdOrderByFormFieldId_OrderAsc(Long formId);
+
+    // formId로 전부 삭제
+    void deleteByFormFieldId_FormId(Long formId);
 }
