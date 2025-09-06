aaa60d2a5e34a8a7415da146a8babb96b6783eca
minjeong204 <anjell1218@gmail.com>
2025-09-06 20:58:07 +0900

    🚀: User Update 기능 추가

---
+++ src/main/java/com/verdianc/wisiee/Entity/UserEntity.java
@@ -0,0 +1,56 @@
+package com.verdianc.wisiee.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+//동일한 provider + providerId는 복수개 존재할 수 없음
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "USER_INFO",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "providerInfo",
+                        columnNames = {"provider_name", "provider_id"}
+                )
+        })
+@Builder
+@Getter
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    //소셜 로그인 provider 정보
+    @Column(name = "provider_name", nullable = false)
+    private String providerNm;
+
+    //소셜 로그인 provider에서 제공하는 providerId
+    //신규 사용지 <-> 기존 사용자 구분키 위해
+    @Column(name = "provider_id", nullable = false)
+    private String providerId;
+
+    @Column(name = "nick_name", nullable = true, unique = true, length = 50)
+    private String nickNm;
+
+    @Column(name = "email")
+    private String email;
+
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_image_id") // FK
+    private FileEntity profileImg;
+}
--- src/main/java/com/verdianc/wisiee/Mapper/UserMapper.java
+++ src/main/java/com/verdianc/wisiee/Mapper/UserMapper.java
@@ -1,18 +1,9 @@
 package com.verdianc.wisiee.Mapper;
 
 import com.verdianc.wisiee.DTO.User.UserInfoUpdateDTO;
-import com.verdianc.wisiee.DTO.User.UserResponseDTO;
 import com.verdianc.wisiee.Entity.UserInfo;
 
 public class UserMapper {
-    public static UserResponseDTO toDTO(UserInfo userInfo) {
-        if (userInfo==null) return null;
-        return UserResponseDTO.builder()
-                .email(userInfo.getEmail())
-                .nickNm(userInfo.getNickNm())
-                .profileImg(userInfo.getProfileImgUrl())
-                .build();
-    }
 
     public static UserInfo toDTO(UserInfoUpdateDTO userInfoUpdateDTO) {
         if (userInfoUpdateDTO==null) return null;
