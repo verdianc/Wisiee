8f848af7456466adbdfd1a11265b03d1b219b650
minjeong204 <anjell1218@gmail.com>
2025-09-06 20:58:07 +0900

    🚀: User Update 기능 추가

---
+++ src/main/java/com/verdianc/wisiee/Service/Interface/UserService.java
@@ -0,0 +1,9 @@
+package com.verdianc.wisiee.Service.Interface;
+
+import com.verdianc.wisiee.DTO.User.UserInfoUpdateDTO;
+
+public interface UserService {
+    //NickNm Update
+    public void updateUserNickNm(UserInfoUpdateDTO updateUserInfo);
+
+}
