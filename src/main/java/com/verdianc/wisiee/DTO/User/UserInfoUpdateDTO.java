b3548573e5d2fbfa5b89045dd17c12477093b50c
minjeong204 <anjell1218@gmail.com>
2025-09-14 00:00:07 +0900

    🚀 : User Profile Update 개편 및 주소록 엔티티 추가

--- src/main/java/com/verdianc/wisiee/DTO/User/UserInfoUpdateDTO.java
+++ src/main/java/com/verdianc/wisiee/DTO/User/UserInfoUpdateDTO.java
@@ -1,12 +1,47 @@
 package com.verdianc.wisiee.DTO.User;
 
+import java.io.IOException;
+import java.util.Optional;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 @Getter
+@Setter
 @Builder
 public class UserInfoUpdateDTO {
+
+    private Long userId;
     private String nickNm;
     private String email;
-    private String profileImg;
+    private byte[] fileData;
+    private String contentType;
+    private String bankName;
+    private String accountNumber;
+    private String accountHolder;
+
+    // Optional getter
+    public Optional<String> getNickNmOpt() {
+        return Optional.ofNullable(nickNm);
+    }
+
+    public Optional<String> getBankNameOpt() {
+        return Optional.ofNullable(bankName);
+    }
+
+    public Optional<String> getAccountNumberOpt() {
+        return Optional.ofNullable(accountNumber);
+    }
+
+    public Optional<String> getAccountHolderOpt() {
+        return Optional.ofNullable(accountHolder);
+    }
+
+    public void setFile(MultipartFile file) throws IOException {
+        if (file!=null && !file.isEmpty()) {
+            this.fileData = file.getBytes();
+            this.contentType = file.getContentType();
+        }
+    }
 }
