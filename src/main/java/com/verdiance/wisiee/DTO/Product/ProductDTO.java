158e9dc495d811e6594491e3ddf85682676240dc
suahlingo <sgua0720@gmail.com>
2025-12-29 20:54:50 +0900

    🚀:fix build, dto import

--- src/main/java/com/verdiance/wisiee/DTO/Product/ProductDTO.java
+++ src/main/java/com/verdiance/wisiee/DTO/Product/ProductDTO.java
@@ -5,7 +5,6 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.checkerframework.checker.units.qual.A;
 
 @NoArgsConstructor
 @AllArgsConstructor
