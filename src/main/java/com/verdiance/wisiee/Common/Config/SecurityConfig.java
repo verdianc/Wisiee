ca5963ea7790c2185d7fc2b2f8961bd09ae31433
minjeong204 <anjell1218@gmail.com>
2026-01-01 15:23:57 +0900

    🚧: 쿠키 설정하도록 redirect uri 변경

--- src/main/java/com/verdiance/wisiee/Common/Config/SecurityConfig.java
+++ src/main/java/com/verdiance/wisiee/Common/Config/SecurityConfig.java
@@ -6,10 +6,12 @@ import com.verdiance.wisiee.Oauth.OAuth2LoginSuccessHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -25,6 +27,9 @@ public class SecurityConfig {
 
         http
                 .csrf(csrf -> csrf.disable())
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/**", "/wisiee/**", "/wisiee/docs",
                                 "/wisiee/swagger-ui/**",
